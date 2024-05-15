ae09a2a289396404f78b96254542812a495cb6ec
Bruno Davis <91693335+brunodavissr@users.noreply.github.com>
2024-05-15 20:04:00 -0300

    Feito correções

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,14 +1,14 @@
 CREATE TABLE "unidades_atendimento" (
-  "codigo" INTEGER(3) NOT NULL,
+  "codigo" INTEGER NOT NULL,
   "nome" VARCHAR(50) NOT NULL UNIQUE,
-  "endereco" VARCHAR(200) NOT NULL
+  "endereco" VARCHAR(200) NOT NULL,
   PRIMARY KEY("codigo")
 );
 
 CREATE TABLE "telefones_unidades" (
-  "codigo_unidade" INTEGER(3) NOT NULL,
-  "telefone" INTEGER(10) NOT NULL,
-  PRIMARY KEY("codigo_unidade", "telefone")
+  "codigo_unidade" INTEGER NOT NULL,
+  "telefone" INTEGER NOT NULL,
+  PRIMARY KEY("codigo_unidade", "telefone"),
   FOREIGN KEY("codigo_unidade") REFERENCES "unidades_atendimento"("codigo")
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE "unidades_academicas" (
 );
 
 CREATE TABLE "cursos" (
-  "codigo" INTEGER(3) NOT NULL,
+  "codigo" INTEGER NOT NULL,
   "nome" VARCHAR(50) NOT NULL UNIQUE,
   PRIMARY KEY("codigo")
 );
@@ -31,87 +31,87 @@ CREATE TABLE "disciplinas" (
 );
 
 CREATE TABLE "funcionarios_biblioteca" (
-  "matricula" INTEGER(5) NOT NULL,
+  "matricula" INTEGER NOT NULL,
   "nome" VARCHAR(50) NOT NULL,
-  "tipo_funcionario" CHAR(1) NOT NULL CHECK("tipo_funcionario" IN ("A", "B")),
-  PRIMARY KEY("codigo")
+  "tipo_funcionario" CHAR(1) NOT NULL CHECK("tipo_funcionario" IN ('A', 'B')),
+  PRIMARY KEY("matricula")
 );
 
 CREATE TABLE "titulos" (
-  "isbn" INTEGER(5) NOT NULL,
+  "isbn" INTEGER NOT NULL,
   "nome_titulo" VARCHAR(100) NOT NULL,
   "area_principal" VARCHAR(100) NOT NULL,
   "assunto" VARCHAR(100) NOT NULL,
-  "ano_publicacao" INTEGER(4) NOT NULL,
+  "ano_publicacao" INTEGER NOT NULL,
   "editora" VARCHAR(50) NOT NULL,
   "idioma" CHAR(1) NOT NULL,
-  "prazo_emprestimo_professor" INTEGER(2) NOT NULL,
-  "prazo_emprestimo_aluno" INTEGER(2) NOT NULL,
-  "numero_max_renovacao" INTEGER(2) NOT NULL,
-  "edicao" INTEGER(3) NULL,
-  "periodicidade" CHAR(15) NULL CHECK("periodicidade" IN ("SEMANAL", "QUINZENAL", "MENSAL", "TRIMESTRAL", "QUADRIMESTRAL", "SEMESTRAL", "ANUAL")),
-  "tipo_periodico" CHAR(1) NULL CHECK("tipo" IN ("J", "R", "B")),
-  "tipo_titulo" CHAR(1) NOT NULL CHECK("tipo_titulo" IN ("L", "P")),
+  "prazo_emprestimo_professor" INTEGER NOT NULL,
+  "prazo_emprestimo_aluno" INTEGER NOT NULL,
+  "numero_max_renovacao" INTEGER NOT NULL,
+  "edicao" INTEGER NULL,
+  "periodicidade" CHAR(15) NULL CHECK("periodicidade" IN ('SEMANAL', 'QUINZENAL', 'MENSAL', 'TRIMESTRAL', 'QUADRIMESTRAL', 'SEMESTRAL', 'ANUAL')),
+  "tipo_periodico" CHAR(1) NULL CHECK("tipo_periodico" IN ('J', 'R', 'B')),
+  "tipo_titulo" CHAR(1) NOT NULL CHECK("tipo_titulo" IN ('L', 'P')),
   PRIMARY KEY("isbn")
   --Adicionar check para verificar se cada tipo de entidade está preenchida com seus dados corretamente.
 );
 
 CREATE TABLE "autores" (
-  "isbn_titulo" INTEGER(5) NOT NULL,
+  "isbn_titulo" INTEGER NOT NULL,
   "autor" VARCHAR(100) NOT NULL,
   PRIMARY KEY("isbn_titulo", "autor"),
   FOREIGN KEY("isbn_titulo") REFERENCES "titulos"("isbn")
 );
 
 CREATE TABLE "areas_secundarias" (
-  "isbn_titulo" INTEGER(5) NOT NULL,
+  "isbn_titulo" INTEGER NOT NULL,
   "area_secundaria" VARCHAR(100) NOT NULL,
   PRIMARY KEY("isbn_titulo","area_secundaria"),
   FOREIGN KEY("isbn_titulo") REFERENCES "titulos"("isbn")
 );
 
 CREATE TABLE "usuarios_biblioteca" (
-  "codigo" INTEGER(5) NOT NULL,
+  "codigo" INTEGER NOT NULL,
   "nome" VARCHAR(50) NOT NULL,
   "identidade" CHAR(12) NULL,
   "cpf" CHAR(14) NULL,
   "endereco" VARCHAR(100) NOT NULL,
-  "sexo" CHAR(1) NOT NULL CHECK("sexo" IN ("M", "F")),
+  "sexo" CHAR(1) NOT NULL CHECK("sexo" IN ('M', 'F')),
   "data_nascimento" DATE NOT NULL,
-  "estado_civil" CHAR(1) NOT NULL CHECK("estado_civil" IN ("C", "S", "D", "V")),
-  "matricula_professor" INTEGER(5) NULL UNIQUE,
-  "tipo_usuario" CHAR(1) NOT NULL CHECK("tipo_usuario" IN ("A", "P")),
+  "estado_civil" CHAR(1) NOT NULL CHECK("estado_civil" IN ('C', 'S', 'D', 'V')),
+  "matricula_professor" INTEGER NULL UNIQUE,
+  "tipo_usuario" CHAR(1) NOT NULL CHECK("tipo_usuario" IN ('A', 'P')),
   PRIMARY KEY ("codigo")
   --Adicionar verificação para caso usuário seja professor, ter a mátricula preenchida obrigatoriamente
 );
 
 CREATE TABLE "telefones_usuarios" (
-  "codigo_usuario" INTEGER(5) NOT NULL,
-  "telefone" INTEGER(10) NOT NULL,
+  "codigo_usuario" INTEGER NOT NULL,
+  "telefone" INTEGER NOT NULL,
   PRIMARY KEY("codigo_usuario", "telefone"),
   FOREIGN KEY("codigo_usuario") REFERENCES "usuarios_biblioteca"("codigo")
 );
 
 CREATE TABLE "transacoes" (
-  "numero_transacao" INTEGER(9) NOT NULL,
+  "numero_transacao" INTEGER NOT NULL,
   "data_transacao" DATE NOT NULL DEFAULT CURRENT_DATE,
   "horario_transacao" TIME NOT NULL DEFAULT CURRENT_TIME,
-  "tipo_transacao" CHAR(10) NOT NULL CHECK("tipo_transacao" IN ("EMPRESTIMO", "DEVOLUÇÃO", "RENOVAÇÃO", "RESERVA")),
+  "tipo_transacao" CHAR(10) NOT NULL CHECK("tipo_transacao" IN ('EMPRESTIMO', 'DEVOLUÇÃO', 'RENOVAÇÃO', 'RESERVA')),
   PRIMARY KEY("numero_transacao")
 );
 
 CREATE TABLE "copias_titulos" (
-  "numero_copia" INTEGER(5) NOT NULL,
-  "isbn_titulo" INTEGER(5) NOT NULL,
-  "secao_copia" INTEGER(4) NOT NULL,
-  "estante_copia" INTEGER(3) NOT NULL,
+  "numero_copia" INTEGER NOT NULL,
+  "isbn_titulo" INTEGER NOT NULL,
+  "secao_copia" INTEGER NOT NULL,
+  "estante_copia" INTEGER NOT NULL,
   PRIMARY KEY("numero_copia", "isbn_titulo"),
   FOREIGN KEY("isbn_titulo") REFERENCES "titulos"("isbn")
 );
 
 CREATE TABLE "itens_emprestimo" (
-  "numero_item" INTEGER(1) NOT NULL,
-  "numero_transacao" INTEGER(9) NOT NULL,
+  "numero_item" INTEGER NOT NULL,
+  "numero_transacao" INTEGER NOT NULL,
   "data_limite_devolucao" DATE NOT NULL,
   PRIMARY KEY("numero_item", "numero_transacao"),
   FOREIGN KEY("numero_transacao") REFERENCES "transacoes"("numero_transacao")
